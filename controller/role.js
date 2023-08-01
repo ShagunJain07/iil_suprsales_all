cab5c08cdb1d351a9c9434ae50755dc5cbb4998f
E1068 <ShagunJai>
2023-08-01 11:48:07 +0530

    index

--- controller/role.js
+++ controller/role.js
@@ -30,33 +30,45 @@ const api_update=async(req,res)=>{
 }
 
 const index=async(req,res)=>{
-        const role_data=await role_master.find({})
-        const array=[]
-        for(i=0;i<role_data.length;i++){
+      const array=[]
+
+      const distinct_roles=await role_master.distinct("ROLE_ID")
+      //console.log(distinct_roles)
+      for(a=0;a<distinct_roles.length;a++){
+        const role_data=await role_master.find({ROLE_ID:distinct_roles[a]})
             // console.log(role_data[i].AUTH_ID)
            const data={
-            ROLE_ID:role_data[i].ROLE_ID,
-            ROLE_NAME:role_data[i].ROLE_NAME,
-            ROLE_DESCRIPTION:role_data[i].ROLE_DESCRIPTION,
-            FLAG:role_data[i].FLAG,
-            AUTH_ID:role_data[i].AUTH_ID,
+            ROLE_ID:role_data[0].ROLE_ID,
+            ROLE_NAME:role_data[0].ROLE_NAME,
+            ROLE_DESCRIPTION:role_data[0].ROLE_DESCRIPTION,
+            FLAG:role_data[0].FLAG,
+           
             AUTH:[]
            }
-            array.push(data)
-      }
-            for(j=0;j<array.length;j++){
+          
+      
+           
                   // console.log(array[j])
-           const auth_data=await authorization_master_tab.find({AUTH_ID:array[j].AUTH_ID})
+                  const auth_array=await role_master.find({ROLE_ID:role_data[0].ROLE_ID})
+                  const array1=[]
+                  for(n=0;n<auth_array.length;n++){
+                        array1.push(auth_array[n].AUTH_ID)
+                  }
+           const auth_data=await authorization_master_tab.find({AUTH_ID:{$in:array1}})
       //      console.log(auth_data)
-           if(auth_data.length!=0){
-           array[j].AUTH.push({
-                        AUTH_NAME:auth_data[0].AUTH_NAME,
-                        AUTH_ID:auth_data[0].AUTH_ID,
-                        FLAG:auth_data[0].FLAG
-                     })
-            }
+      for(m=0;m<auth_data.length;m++){
+            if(auth_data.length!=0){
+                  data.AUTH.push({
+                               AUTH_NAME:auth_data[m].AUTH_NAME,
+                               AUTH_ID:auth_data[m].AUTH_ID,
+                               FLAG:auth_data[m].FLAG
+                            })
+                   }
+      
       }
-            
+      array.push(data)
+
+} 
         
 
         //console.log(array)
